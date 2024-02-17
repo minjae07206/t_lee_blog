aca19425b8cbbd251ebb43ccb822ba9a2fc910a3
minjae07206 <minjaekor@gmail.com>
2024-02-17 22:49:27 +0400

    initial setup

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "index.html")
